03ed4ea3d3ded933ffb1707bd86fbff2621af142
Giacomo Ghinelli <111856732+GiacomoGhi@users.noreply.github.com>
2026-06-08 06:57:25 +0200

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int MaxDepth(TreeNode root)
+        => dfs(root);
+
+    int dfs(TreeNode node)
+    {
+        if (node is null)
+        {
+            return 0;
+        }
+
+        return 1 + Math.Max(dfs(node.left), dfs(node.right));
+    }
+}
